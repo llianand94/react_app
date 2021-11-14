f37214c297e491379eb43e0f4632c6d1142ab10b
llianand94 <89574095+llianand94@users.noreply.github.com>
2021-11-14 20:31:55 +0200

    Add Link component in a App Component

--- src/App.js
+++ src/App.js
@@ -1,72 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import React, {Component} from 'react';
-import Ciao from './components/Ciao';
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      isDirection:true,
-      users:[
-        {
-          id:1,
-          fname:'Elon'
-        },
-        {
-          id:2,
-          fname:'Elen'
-        },
-        {
-          id:3,
-          fname:'Tim'
-        },
-        {
-          id:4,
-          fname:'Bob'
-        },
-        {
-          id:5,
-          fname:'Sasha'
-        },
-        {
-          id:6,
-          fname:'Rob'
-        },
-        {
-          id:7,
-          fname:'Alex'
-        },
-        {
-          id:8,
-          fname:'Rex'
-        },
-      ]
-    }
-  }
-  sortUsers = () =>{
-    const {users,isDirection} = this.state;
-    const sortUsers = JSON.parse(JSON.stringify(users));
-    //const sortUsers = [...users];
-    sortUsers.sort((prev, next)=> isDirection ? next.id-prev.id:prev.id-next.id);
-    this.setState({
-      isDirection:!isDirection,
-      users:sortUsers
-    })
-  }
-  sortUsersAlpha = () =>{
-    
-  }
+
+import Link from './Component/Link/index';
+
+
+class App extends React.Component{
   render(){
-    const {users,isDirection} = this.state;
     return <>
-    <p>
-    <button onClick={this.sortUsers}>SORT BY NUMBER {isDirection?'straight ':'reverse '}</button>
-    <button onClick={this.sortUsersAlpha}>SORT BY NAME {isAlphabet?'straight ':'reverse '}</button>
-    </p>
-    <div>
-      {users.map(({id, fname})=><Ciao key={id}  id={id} name={fname} />)}
-    </div>
+      <Link className="some-link" href="#"> first link</Link>
+      <Link className="some-link" href="#"> example text of link</Link>
     </>;
-  }
+  }    
 }
 export default App;
